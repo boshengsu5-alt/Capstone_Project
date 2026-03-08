487245e5708b64d838692931d3c28a5245533880
boshengsu5-alt <boshengsu5@gmail.com>
2026-03-08 17:02:59 +0200

    chore(mobile): temporarily route to auth screens for testing

--- app-mobile/src/navigation/RootNavigator.tsx
+++ app-mobile/src/navigation/RootNavigator.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import MainTabNavigator from './MainTabNavigator';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/auth/LoginScreen';
+import RegisterScreen from '../screens/auth/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
     return (
         <NavigationContainer>
-            <MainTabNavigator />
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
